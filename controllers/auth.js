7166dd99ad802f2212e1f179bb013912a88a3376
Mike Houser <saintmikel2004@gmail.com>
2020-07-18 14:36:45 -0400

    push

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const expressJwt = require('express-jwt');
